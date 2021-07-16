3df789ee312dd59d3301245c63b61fc4db1bb450
camilopgonzalez <jameskairlord@live.com>
2021-07-16 16:56:28 +0200

    post crud

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+
+        return View::make('Post.IndexPost')
+            ->with('posts', $posts);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return View::make('Post.CreatePost');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $post = new Post;
+        $post->content = $request->content;
+        $post->save();
+
+        Session::flash('message', 'Post Successfully created!');
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        //$post = Post::find($id);
+
+        return View::make('Post.ShowPost')
+            ->with('post', $post);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        //$post = Post::find($id);
+
+        return View::make('Post.EditPost')
+            ->with('post', $post);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        $post->content = $request->content;
+        $post->save();
+
+        Session::flash('message', 'Post Successfully updated!');
+        return redirect()->back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        Session::flash('message', 'Post Successfully deleted!');
+        return View::make('Post.IndexPost')
+            ->with('posts', $posts);
+    }
+}
